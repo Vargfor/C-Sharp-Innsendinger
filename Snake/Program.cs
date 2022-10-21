6737c9db25ba9313f81a2926c9022c26ad6a3d6f
Henrik <102602537+Vargfor@users.noreply.github.com>
2022-10-21 14:43:34 +0200

    Add files via upload

---
+++ Snake/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snake1
+{
+
+    public  partial class Snake : Form
+    {
+        int cols = 50, rows = 25, score = 0, dx = 0, dy = 0, front = 0, back = 0;
+        Piece[] snake = new Piece[1250];
+        List<int> available = new List<int>();
+        bool[,] visit;
+
+        Random rand = new Random();
+
+        Timer timer = new Timer();
+
+        public Snake()
+        {
+            InitializeComponent();
+            initial();
+            launchTimer();
+        }
+
+        private void launchTimer()
+        {
+            timer.Interval = 50;
+            timer.Tick += move;
+            timer.Start();
+        }
+
+        private void Snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            dx = dy = 0;
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    dx = 20;
+                    break;
+                case Keys.Left:
+                    dx = -20;
+                    break;
+                case Keys.Up:
+                    dy = -20;
+                    break;
+                case Keys.Down:
+                    dy = 20;
+                    break;
+            }
+        }
+
+        private void move(object sender, EventArgs e)
+        {
+            int x = snake[front].Location.X, y = snake[front].Location.Y;
+            if (dx == 0 && dy == 0) return;
+            if (game_over(x + dx, y + dy))
+            {
+                timer.Stop();
+                MessageBox.Show("Game Over");
+                return;
+            }
+
+        }
+    }
+}
